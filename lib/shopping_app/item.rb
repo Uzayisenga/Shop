b8e1cad8c515fae6d147f3d75dd051fae56c49c5
Uzayisenga <aminauzayisenga@gmail.com>
2022-03-21 13:13:48 +0200

    add checkout

---
+++ lib/shopping_app/item.rb
@@ -0,0 +1,28 @@
+require_relative 'ownable'
+
+class Item
+  include Ownable
+
+  attr_reader :name, :price
+
+  @@instances = []
+
+  def initialize(name, price, owner=nil)
+    @name = name
+    @price = price
+    self.owner = owner
+
+    
+    @@instances << self
+  end
+
+  def label
+    { name: name, price: price }
+  end
+
+  def self.all
+    
+    @@instances
+  end
+
+end
